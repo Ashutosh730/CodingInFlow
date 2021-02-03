02a33bf89d3dd6ed137a811d818d511e84aa8a75
Ashutosh Kumar <ashutoshhappy0007@gmail.com>
2021-02-03 20:24:56 +0530

    Volley Library

--- app/src/main/java/com/example/json_gson_java/MainActivity.java
+++ app/src/main/java/com/example/json_gson_java/MainActivity.java
@@ -1,30 +1,71 @@
 package com.example.json_gson_java;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
-//        Employee employee = new Employee("Ashutosh", "Kumar" , 21, "fgdgf5");
-//        String json = gson.toJson(employee);
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
 
-        String json = "{\"age\":21,\"first_name\":\"Ashutosh\",\"last_name\":\"Kumar\",\"password\":\"fgdgf5\"}";
-        Employee employee = gson.fromJson(json, Employee.class);
+        mQueue = Volley.newRequestQueue(this);
 
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
+    }
+
+    private void jsonParse() {
+
+        String url = "https://api.androidhive.info/volley/person_object.json";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String name = null;
+                        try {
+                            name = response.getString("name");
+                            String email = response.getString("email");
+                            mTextViewResult.append(name + ", " + ", " + email + "\n\n");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
     }
 }
